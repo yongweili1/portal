681990ddfce0b78f244449e65c6f06371fa7d36e
yan.liu01_UIH-AI <yan.liu01@united-imaging.com>
2018-11-06 15:24:57 +0800

    add a table ROI

--- doc/db_create.sql
+++ doc/db_create.sql
@@ -182,6 +182,22 @@ create table new_django_session
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+create table roi
+(
+	pid                 int auto_increment,
+	roiname             varchar(64) not null,
+    seriesuid           varchar(64) not null,
+	roicolor            varchar(64) not null,
+    importdatatime		datetime,
+    updatetime          timestamp not null default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+	primary key (pid),
+    unique key (pid),
+    foreign key (seriesuid) references series(seriesuid) on delete cascade
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 /*
 create table role
 (
