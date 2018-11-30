6d167311e6c9655203b82d5615ff1869b5019619
jiuwen.jiang <jiuwen.jiang@united-imaging.com>
2018-11-30 16:21:12 +0800

    修复多次订阅的bug

--- front_end/src/app/components/contouring/slider-bar/slider-bar.component.ts
+++ front_end/src/app/components/contouring/slider-bar/slider-bar.component.ts
@@ -1,27 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { EventAggregator } from '../../../shared/common/event_aggregator';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'mpt-slider-bar',
     templateUrl: './slider-bar.component.html',
     styleUrls: ['./slider-bar.component.less']
 })
-export class SliderBarComponent implements OnInit {
+export class SliderBarComponent implements OnInit, OnDestroy {
     progress: number;
 
     totalSliceCount: number;
     currentSliceNum: number;
+
+    volumnSizeSubscriber: Subscription;
+    sliceIndexSubscriber: Subscription;
+
+    @Input() tag: string;
 
     constructor() {
         this.totalSliceCount = 0;
         this.currentSliceNum = 0;
         this.progress = 100;
 
-        EventAggregator.Instance().volumnSize.subscribe(value => {
+        this.volumnSizeSubscriber = EventAggregator.Instance().volumnSize.subscribe(value => {
             this.totalSliceCount = value[2];
         });
 
-        EventAggregator.Instance().sliceIndex.subscribe(value => {
+        this.sliceIndexSubscriber = EventAggregator.Instance().sliceIndex.subscribe(value => {
             this.currentSliceNum = value;
             this.progress = this.currentSliceNum / this.totalSliceCount * 100;
             console.log('Current progress', this.progress);
@@ -31,13 +37,18 @@ export class SliderBarComponent implements OnInit {
     ngOnInit() {
     }
 
+    ngOnDestroy() {
+        this.volumnSizeSubscriber.unsubscribe();
+        this.sliceIndexSubscriber.unsubscribe();
+    }
+
     // Callback to invoke on value change via slide.
     handleChange(e) {
         console.log('handleChange', e.value);
         const newIndex = e.value / 100 * this.totalSliceCount;
         const pageDelta = Math.floor(newIndex - this.currentSliceNum);
         this.currentSliceNum = newIndex;
-        EventAggregator.Instance().pageDelta.publish(pageDelta);
+        EventAggregator.Instance().pageDelta.publish([this.tag, pageDelta]);
     }
 
     // Callback to invoke when slide stops.
