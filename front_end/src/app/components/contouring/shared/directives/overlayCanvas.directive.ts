e0f0bf7b1f011c1c54a86366617db8226f2f3e38
jiuwen.jiang <jiuwen.jiang@united-imaging.com>
2018-12-07 16:16:06 +0800

    修复action切换逻辑错误

--- front_end/src/app/components/contouring/shared/directives/overlayCanvas.directive.ts
+++ front_end/src/app/components/contouring/shared/directives/overlayCanvas.directive.ts
@@ -128,7 +128,13 @@ export class OverlayCanvasDirective implements OnInit, OnChanges, OnDestroy {
 
         this.stage.children.forEach(shape => {
             if (shape.type === ShapeTypeEnum.freepen) {
-                shape.editable = this.shapeType === ShapeTypeEnum.freepen2 ? true : false;
+                if (this.actionType === ActionTypeEnum.select) {
+                    shape.editable = false;
+                } else if (this.shapeType === ShapeTypeEnum.freepen2) {
+                    shape.editable = true;
+                } else {
+                    shape.editable = false;
+                }
             }
         });
 
