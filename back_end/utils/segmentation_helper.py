c8b8430fa45f2ee7ec73b240e358ef9ccdaf9cd0
jiuwen.jiang <jiuwen.jiang@united-imaging.com>
2018-12-10 14:03:54 +0800

    mask 2 contours

--- back_end/utils/segmentation_helper.py
+++ back_end/utils/segmentation_helper.py
@@ -3,7 +3,6 @@ import os
 import cv2
 import numpy as np
 import md.image3d.python.image3d_io as cio
-from md.image3d.python.image3d_tools import slice_nn
 
 from netbase.c_log import log
 
@@ -22,24 +21,15 @@ class SegmentationHelper(object):
             log.dev_error('Path {} is not a mask file')
             return None
         mask = cio.read_image(mask_fp)
-        center_x, center_y, center_z = mask.center()
-        size_x, size_y, slice_count = mask.size()
-        spacing_x, spacing_y, spacing_z = mask.spacing()
-        origin_x, origin_y, origin_z = mask.origin()
-        axis_x = mask.axis(0)
-        axis_y = mask.axis(1)
-        axis_z = mask.axis(2)
-
         mask.from_numpy(mask.to_numpy(), np.uint8)
+        size_x, size_y, slice_count = mask.size()
+        slices = mask.to_numpy()
 
         contours = {}
         for index in range(slice_count):
             slice_index = index + 1
-
-            look_at = np.array([center_x, center_y, origin_z + index * spacing_z])
-            mask_slice = slice_nn(mask, look_at, axis_x, axis_y, look_at,
-                                  [spacing_x, spacing_y], [size_x, size_y], -1024)
-            _, thresh = cv2.threshold(mask_slice, 0, 255, cv2.THRESH_BINARY)
+            slice = slices[index]
+            _, thresh = cv2.threshold(slice, 0, 255, cv2.THRESH_BINARY)
             _, contour, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             if len(contour) == 0:
                 continue
@@ -49,8 +39,9 @@ class SegmentationHelper(object):
                 graphic = []
                 for i in item:
                     pt = i[0].tolist()
-                    pt = [pt[0] + origin_x, pt[1] + origin_y, origin_z + index * spacing_z]
-                    graphic.append(pt)
+                    pt.append(index)
+                    pt = mask.voxel_to_world(pt)
+                    graphic.append(pt.tolist())
                 graphics.append(graphic)
 
             contours[slice_index] = graphics
