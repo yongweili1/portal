9113fcd461c5e4df2ca48d250a71be52180e2de3
yongwei.li <yongwei.li@united-imaging.com>
2018-12-13 20:01:30 +0800

    finish center_node code

--- netbase/cmd_event_id.py
+++ netbase/cmd_event_id.py
@@ -4,6 +4,11 @@ class CmdId(object):
     cmd_id_report_gpu_info = 101
     cmd_id_report_srv_info = 102
     cmd_id_restart_algor_srv = 103
+    cmd_id_run_algor_srv = 104
+    cmd_id_restart_algor_srv = 105
+    cmd_id_find_algor_srv = 106
+    cmd_id_find_render_srv = 107
+    cmd_id_release_render_srv = 108
 
 
 class EventId(object):
